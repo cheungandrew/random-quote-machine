c6af955ff50bbae824e5b94d13985bf3f0cd9393
Andrew Cheung <cheungandrew5@gmail.com>
2017-11-10 20:54:18 -0600

    Add background images

--- quote.js
+++ quote.js
@@ -2,10 +2,15 @@ function randomQuoteGen() {
     // URL of random quote API
     var quoteapi = "https://talaikis.com/api/quotes/random/";
 
+    // Background images
+    var images = ["images/lake.jpg", "images/mountain.jpg", "images/river.jpg"];
+    var imgNum = 1;
+
     // Getting JSON of quote and writing it to HTML
     $.getJSON(quoteapi, function(data) {
         $(".mb-0").html('"' + data.quote + '"');
         $(".blockquote-footer").html(data.author);
+
         // Tweet
         $(".tweet").prop("href", "https://twitter.com/intent/tweet?text=" + data.quote);
     });
@@ -15,8 +20,16 @@ function randomQuoteGen() {
         $.getJSON(quoteapi, function(data) {
             $(".mb-0").html('"' + data.quote + '"');
             $(".blockquote-footer").html(data.author);
+
             // Tweet
             $(".tweet").prop("href", "https://twitter.com/intent/tweet?text=" + data.quote);
+
+            // Change background image
+            $("body").css("background-image", "url(" + images[imgNum] +")");
+            imgNum++;
+            if (imgNum === 3) {
+                imgNum = 0;
+            }
         });
     });
 }
